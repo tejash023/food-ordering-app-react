970e0218f6c96dbf9f963e76942654b451cbcbbf
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-03-03 13:34:43 +0530

    added hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,48 @@
-import { restaurants } from "../constants";
-import RestaurantCards from "./ResturantCard";
+import { useState } from "react";
+import { restaurantsList } from "../constants";
+import RestaurantCards from "./RestaurantCard";
+
+function filterRestaurant(searchText, restaurants) {
+  console.log(searchText, restaurants);
+  const filteredData = restaurants.filter((restaurant) =>
+    restaurant.data.name.toLowerCase().includes(searchText.toLowerCase)
+  );
+  console.log(filteredData);
+  return filteredData;
+}
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantsList);
+  const [searchInput, setSearchInput] = useState("");
+
   return (
-    <div className="restaurant-lists">
-      {restaurants.map((restaurant) => (
-        <RestaurantCards {...restaurant.data} />
-      ))}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterRestaurant(searchInput, restaurants);
+            // update the state - restuarants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-lists">
+        {restaurants.map((restaurant) => (
+          <RestaurantCards {...restaurant.data} />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-export const restaurants = [
+export const restaurantsList = [
   {
     type: "restaurant",
     data: {
