bca69484f06e288c920ca750be6d3d657b455c51
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-03-25 21:28:42 +0530

    added category option in menu

--- src/components/RestaurantCategory/RestaurantItemCategory.js
+++ src/components/RestaurantCategory/RestaurantItemCategory.js
@@ -1,10 +1,23 @@
 import RestaurantMenu from "../RestaurantMenu";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { useState } from "react";
 
 const RestaurantItemCategory = ({ itemCategory }) => {
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <div className="item-category">
-      <p>{itemCategory.title}</p>
-      <RestaurantMenu menuItems={itemCategory.itemCards} />
+      <div className="item-category-header">
+        <p>{itemCategory.title}</p>
+
+        {isVisible ? (
+          <FaChevronUp onClick={() => setIsVisible(false)} />
+        ) : (
+          <FaChevronDown onClick={() => setIsVisible(true)} />
+        )}
+      </div>
+      {!isVisible && <div className="section-border"></div>}
+
+      {isVisible && <RestaurantMenu menuItems={itemCategory.itemCards} />}
     </div>
   );
 };
