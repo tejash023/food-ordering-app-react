ce815670b09c8afceabb86d043468e6e346d256d
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-04-06 19:40:24 +0530

    updated css for small devices

--- src/components/Error.js
+++ src/components/Error.js
@@ -4,11 +4,13 @@ import img from "../assets/img/mars.png";
 const Error = () => {
   const err = useRouteError();
   return (
-    <div className="error-container">
+    <div className="container">
       <div className="error-page">
-        <img className="error-img" src={img} />
+        <img className="error-img" alt="error-img" src={img} />
         <h1>Oops!!</h1>
-        <h2>You might have landed on a different planet!</h2>
+        <p className="subheading-text">
+          You might have landed on a different planet!
+        </p>
         {err && <p>{err.status + " : " + err.statusText}</p>}
         <button className="error-button">
           <Link to="/">Back to Home</Link>
