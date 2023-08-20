5878ac82710dd796f271c6bc0f0aee0e5dc04786
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-08-20 23:41:55 +0530

    updated about us section

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -3,14 +3,17 @@ import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 const FAQ = ({ title, description, isVisible, setIsVisible }) => {
   return (
     <div className="faq-section mtop20">
-      <div className="faq-title mtop20">
-        <p>{title}</p>
-        {isVisible ? (
-          <FaChevronUp onClick={() => setIsVisible(false)} />
-        ) : (
-          <FaChevronDown onClick={() => setIsVisible(true)} />
-        )}
-      </div>
+      {isVisible ? (
+        <div className="faq-title mtop20" onClick={() => setIsVisible(false)}>
+          <p>{title}</p>
+          <FaChevronUp />
+        </div>
+      ) : (
+        <div className="faq-title mtop20" onClick={() => setIsVisible(true)}>
+          <p>{title}</p>
+          <FaChevronDown />
+        </div>
+      )}
 
       <div className="faq-content">{isVisible && <p>{description}</p>}</div>
     </div>
