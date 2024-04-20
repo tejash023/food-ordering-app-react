fcf972f845fd0c307efdffcbdb6ab1db6e23f234
Tejashwa Rajvardhan <keshu023@gmail.com>
2024-04-20 21:47:39 +0530

    fixed image error issue

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const FETCH_RESTAURANTS =
   "https://gofoodsserver.onrender.com/api/restaurants/?lat=12.9715987&lng=77.5945627";
 
 export const IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const FETCH_MENU_URL =
   "https://gofoodsserver.onrender.com/api/menu/?lat=25.5940947&lng=85.1375645&restaurantId=";
